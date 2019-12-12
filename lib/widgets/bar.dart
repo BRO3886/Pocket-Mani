944dac22bbeb8ee12a5b8b493f4a2ffad162302d
siddhartha <siddverma1999@gmail.com>
2019-12-12 12:53:45 +0530

    added graph to check weekly spending

---
+++ lib/widgets/bar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Bar extends StatelessWidget {
+  final String label;
+  final double amount;
+  final double percentOfTotal;
+
+  Bar(
+      {@required this.label,
+      @required this.amount,
+      @required this.percentOfTotal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text('₹ ${amount.toStringAsFixed(0)}'),
+          SizedBox(
+            height: 5,
+          ),
+          Container(
+            height: 200,
+            width: 10,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey[100], width: 0.5),
+                      color: Colors.grey[50],
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+                FractionallySizedBox(
+                  heightFactor: percentOfTotal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).accentColor,
+                        borderRadius: BorderRadius.circular(20)),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 4,
+          ),
+          Text(label),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/graph.dart
+++ lib/widgets/graph.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/expense.dart';
+import './bar.dart';
 
 class Graph extends StatelessWidget {
   final List<Expense> recentExpenses;
@@ -21,8 +22,16 @@ class Graph extends StatelessWidget {
       print(DateFormat.E().format(weekDay));
       print(totalSum);
 
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    });
+  }
 
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+  double get spending {
+    return groupedExpenseValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
     });
   }
 
@@ -30,15 +39,29 @@ class Graph extends StatelessWidget {
   Widget build(BuildContext context) {
     print(groupedExpenseValues);
     return Container(
-      padding: EdgeInsets.all(6),
-      child: Card(
-        child: Text('Graph'),
-        elevation: 8,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      ),
       width: double.infinity,
       height: 300,
       margin: EdgeInsets.only(top: 12, bottom: 12),
+      padding: EdgeInsets.all(6),
+      child: Card(
+        elevation: 0.5,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: Container(
+          margin: EdgeInsets.all(8),
+          padding: EdgeInsets.all(8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groupedExpenseValues.map((data) {
+              return Bar(
+                amount: data['amount'],
+                label: data['day'],
+                percentOfTotal:
+                    spending == 0 ? 0.0 : (data['amount'] as double) / spending,
+              );
+            }).toList(),
+          ),
+        ),
+      ),
     );
   }
 }
